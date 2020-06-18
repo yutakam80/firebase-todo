47ecb17135d2abb899186a045bd2a8dec53d4678
yutakam80 <yutakam80@gmail.com>
2020-06-18 22:28:06 +0900

    Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,35 @@ import { NextPage } from 'next'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import Router from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 
 const Index: NextPage = () => {
+  const [initialMount, setInitialMount] = useState(false)
   const clickSignIn = () => {
+    sessionStorage.setItem('pending', '1')
     firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider())
   }
   useEffect(() => {
-    firebase.auth().getRedirectResult().then((result) => {
-      if (result.user) {
-        Router.push('/todo')
-      }
-    })
+    if (sessionStorage.getItem('pending')) {
+      sessionStorage.removeItem('pending')
+      Router.push('/todo')
+    } else {
+      setInitialMount(true)
+    }
   }, [])
-  return (
-    <div>
-      <h1>firebase-todo</h1>
-      <button onClick={clickSignIn}>
-        Googleアカウントでログイン
-      </button>
-    </div>
-  )
+  return useMemo(() => {
+    if (!initialMount) {
+      return null
+    }
+    return (
+      <div>
+        <h1>firebase-todo</h1>
+        <button onClick={clickSignIn}>
+          Googleアカウントでログイン
+        </button>
+      </div>
+    )
+  }, [initialMount])
 }
 
 export default Index
