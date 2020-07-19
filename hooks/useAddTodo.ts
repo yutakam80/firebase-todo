55c9edb7a75fc9db1f1dee890525008051cb04bf
Yutaka Moriya <yutakam80@gmail.com>
2020-07-19 10:23:45 +0900

    wip

---
+++ hooks/useAddTodo.ts
@@ -0,0 +1,19 @@
+import { useState, useCallback } from 'react'
+import firebase from 'services/firebase'
+
+export const useAddTodo = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const handleAddTodo = useCallback(async () => {
+    setLoading(true)
+    try {
+      firebase.firestore().collection()
+    } catch(e) {
+      setError(true)
+      setLoading(false)
+    }
+  }, [])
+
+  return { loading, error }
+}
